a4391958180f7bd625f2cb6425b5a9679fb0aeee
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-02-08 13:39:57 +0530

    remove routes

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import { slashRout } from '../controllers/routesController.js';
-
-router.get('/', slashRout());
-
-export default router;
